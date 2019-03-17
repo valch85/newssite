bec68dbfa06202a6f93e15f18c41a848e3843ae3
valch85 <val@valch.name>
2019-03-18 00:52:04 +0200

    - make news in admin show by name filed - add news URL and a view

--- news/models.py
+++ news/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
+    def __str__(self):
+        return self.news_name
     news_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     news_desc = models.TextField(default=None)
