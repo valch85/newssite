fe1b2b5ab06811943cb53386e0ab118385263356
valch85 <val@valch.name>
2019-03-18 02:02:42 +0200

    news main page as a list

--- news/views.py
+++ news/views.py
@@ -1,5 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_news_list = News.objects.order_by('-pub_date')[:5]
+    context = {'latest_news_list': latest_news_list}
+    return render(request, 'news/index.html', context)
+
+
+def detail(request, news_id):
+    #return HttpResponse("You're looking at news %s." % news_id)
+    return HttpResponse("You're looking at news %s." % news_id)
+
